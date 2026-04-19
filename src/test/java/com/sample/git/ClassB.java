8a14910e0cf596c54dd22e85f7c061ff557dd9a1
NithyaKalyani-U <nithyakalyaniulaganathan@gmail.com>
2026-04-19 13:04:51 +0530

    added methods in classA & added classB

---
+++ src/test/java/com/sample/git/ClassB.java
@@ -0,0 +1,15 @@
+package com.sample.git;
+
+public class ClassB {
+	
+	public void java() {
+		System.out.println("m01");
+	}
+	public void python() {
+		System.out.println("m02");
+	}
+	public void ruby() {
+		System.out.println("m03");
+	}
+
+}
