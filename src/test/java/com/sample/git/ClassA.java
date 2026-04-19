f9d8e2d7e1998118f781a72ddd25e9569f1058b1
NithyaKalyani-U <nithyakalyaniulaganathan@gmail.com>
2026-04-19 13:45:40 +0530

    added from tester2

--- src/test/java/com/sample/git/ClassA.java
+++ src/test/java/com/sample/git/ClassA.java
@@ -11,5 +11,14 @@ public class ClassA {
 	public void m03() {
 		System.out.println("m03");
 	}
+	public void tamil() {
+		System.out.println("m01");
+	}
+	public void malayalam() {
+		System.out.println("m02");
+	}
+	public void telugu() {
+		System.out.println("m03");
+	}
 
 }
